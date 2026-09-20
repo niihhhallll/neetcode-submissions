498a3daa97546902b683a5c4008b59dc76b7308b
𝙣𝙞𝙝𝙝𝙖𝙖𝙡𝙡𝙡𝙡𝙡𝙡 <nihal.linux@proton.me>
2026-09-20 10:54:37 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        s.erase(std::remove_if(s.begin(), s.end(), [](unsigned char c) {
+        return !std::isalnum(c); // Remove it if it is NOT alphanumeric
+        }), s.end());
+        std::transform(s.begin(), s.end(), s.begin(), ::tolower);
+
+        int left = 0;
+        int right = s.size() - 1; 
+        while(left < right)
+        {
+            if(s[left] == s[right])
+            {
+                left++; 
+                right--; 
+            }
+            else
+            {
+                return false; 
+            }
+        }
+        return true; 
+    }
+};
